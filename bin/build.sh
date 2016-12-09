2840a38371b6970c018aa6c11a11064660db7dcc
John Paul Hayes II <bwackwat@github.com>
2016-12-09 15:25:01 -0800

    Simplified build, added flag for particular builds.

--- bin/build.sh
+++ bin/build.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
+argc=$#
+argv=$*
+
 compiler="clang++ -std=c++11 -g -O0 \
 -Weverything -Wpedantic \
 -Wno-c++98-compat -Wno-padded \
 -Wno-exit-time-destructors -Wno-global-constructors"
 
 function build {
-	echo -e "\n\n"
-	echo -e "-------------------------BUILDING-------------------------\n"
-	echo "$compiler $1 $2 $3 $4 $5 $6 $7 $8 $9"
-	echo -e "\n\n"
-	eval "$compiler $1 $2 $3 $4 $5 $6 $7 $8 $9"
+	if [ $argc -eq 0 ] || [[ $@ == *$argv* ]]; then 
+		echo -e "\n\n"
+		echo -e "-------------------------BUILDING-------------------------\n"
+		echo "$compiler $2 $3 $4 $5 $6 $7 $8 $9 -o ./bin/$1"
+		echo -e "\n\n"
+		eval "$compiler $2 $3 $4 $5 $6 $7 $8 $9 -o ./bin/$1"
+	fi
 }
 
-build "./src/tcp-poll-server.cpp ./src/util.cpp -o ./bin/tcp-poll-server"
-build "-lpthread ./src/tcp-client.cpp ./src/util.cpp ./src/simple-tcp-client.cpp -o ./bin/tcp-client"
-build "./src/json.cpp ./src/json-test.cpp ./src/util.cpp -o ./bin/json-test"
-build "-lssl -lcrypto ./src/modern-web-monad.cpp ./src/util.cpp ./src/simple-tcp-server.cpp ./src/json.cpp -o ./bin/modern-web-monad"
-build "./src/ponal-client.cpp ./src/util.cpp ./src/simple-tcp-client.cpp -o ./bin/ponal"
-build "./src/ponal-server.cpp ./src/util.cpp ./src/simple-tcp-server.cpp -o ./bin/ponald"
-build "./src/read-stdin-tty.cpp ./src/util.cpp -o ./bin/read-stdin-tty"
-build "-lcryptopp ./src/comd.cpp ./src/comd-util.cpp ./src/simple-tcp-server.cpp ./src/util.cpp -o ./bin/comd"
-build "-lcryptopp ./src/com.cpp ./src/comd-util.cpp ./src/simple-tcp-client.cpp ./src/util.cpp -o ./bin/com"
-build "-lcryptopp ./src/keyfile-gen.cpp -o ./bin/keyfile-gen"
-build "-lpqxx ./src/pgsql-model.cpp ./src/json.cpp ./src/pgsql-model-test.cpp -o ./bin/pgsql-model-test"
+build tcp-poll-server "./src/tcp-poll-server.cpp ./src/util.cpp"
+build tcp-client "-lpthread ./src/tcp-client.cpp ./src/util.cpp ./src/simple-tcp-client.cpp"
+build json-test "./src/json.cpp ./src/json-test.cpp ./src/util.cpp"
+build modern-web-monad "-lssl -lcrypto ./src/modern-web-monad.cpp ./src/util.cpp ./src/simple-tcp-server.cpp ./src/json.cpp"
+build ponal "./src/ponal-client.cpp ./src/util.cpp ./src/simple-tcp-client.cpp"
+build ponald "./src/ponal-server.cpp ./src/util.cpp ./src/simple-tcp-server.cpp"
+build read-stdin-tty "./src/read-stdin-tty.cpp ./src/util.cpp"
+build comd "-lcryptopp ./src/comd.cpp ./src/comd-util.cpp ./src/simple-tcp-server.cpp ./src/util.cpp"
+build com "-lcryptopp ./src/com.cpp ./src/comd-util.cpp ./src/simple-tcp-client.cpp ./src/util.cpp"
+build keyfile-gen "-lcryptopp ./src/keyfile-gen.cpp"
+build pgsql-model-test "-lpqxx ./src/pgsql-model.cpp ./src/json.cpp ./src/pgsql-model-test.cpp"
