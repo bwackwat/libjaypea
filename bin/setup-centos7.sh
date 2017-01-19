1f2261b0b6f216685c1f84a2aa6f60c664d52b91
John Paul Hayes II <bwackwat@github.com>
2017-01-18 18:05:11 -0800

    Setup nits.

--- bin/setup-centos7.sh
+++ bin/setup-centos7.sh
@@ -3,11 +3,13 @@
 yum makecache fast
 yum -y upgrade
 
-yum -y install epel-release clang fail2ban rkhunter certbot
+yum -y install epel-release
+yum -y install clang fail2ban rkhunter certbot
 yum -y install libstdc++-static libstdc++ cryptopp cryptopp-devel openssl openssl-devel
 yum -y install firewalld
 
-firewall-cmd --zone=public --add-service=http --permanent
-firewall-cmd --zone=public --add-service=https --permanent
-firewall-cmd --reload
-systemctl enable firewall-cmd
+systemctl enable fail2ban
+systemctl enable firewalld
+
+rkhunter --update
+rkhunter --propupd
