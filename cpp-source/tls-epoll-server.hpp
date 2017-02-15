51c8ccf4180ba472eb0d757d101c0b250beee249
John Paul Hayes II <bwackwat@github.com>
2017-02-15 09:05:37 -0800

    Nice simplified things. Opening my mind to further simplications. These abstractions really remind be of server programming with node.js.

--- cpp-source/tls-epoll-server.hpp
+++ cpp-source/tls-epoll-server.hpp
@@ -19,6 +19,5 @@ public:
 	~TlsEpollServer();
 
 	bool send(int fd, const char* data, size_t data_length);
-	ssize_t recv(int fd, char* data, size_t data_length);
 	ssize_t recv(int fd, char* data, size_t data_length, std::function<ssize_t(int, char*, size_t)> callback);
 };
