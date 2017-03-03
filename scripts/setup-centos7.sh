b97dcdd7629c30de4977f2da769d7fd6f8245776
Slava Maslennikov <slava.maslennikov@hautelook.com>
2017-03-02 20:36:23 -0800

    make install argon2 because why not

--- scripts/setup-centos7.sh
+++ scripts/setup-centos7.sh
@@ -7,10 +7,16 @@ mkdir -p artifacts
 
 yum -y install epel-release
 
+yum -y install git firewalld
 yum -y install clang gcc-c++ libpqxx-devel
 yum -y install libstdc++-static libstdc++ cryptopp cryptopp-devel openssl openssl-devel
 
-if [ -d "artifacts/argon2"]; then
+systemctl enable fail2ban
+systemctl restart fail2ban
+systemctl enable firewalld
+systemctl restart firewalld
+
+if [ -d "artifacts/argon2" ]; then
 	rm -rf artifacts/argon2
 fi
 
@@ -18,10 +24,3 @@ git clone https://github.com/P-H-C/phc-winner-argon2.git artifacts/argon2
 cd artifacts/argon2
 make
 make install
-#cp -f artifacts/argon2/libargon2.so.0 artifacts/argon2/libargon2.so
-#cp -f artifacts/argon2/libargon2.so.0 /usr/local/lib/
-#cp -f artifacts/argon2/libargon2.so.0 /lib/
-#cp -f artifacts/argon2/libargon2.so /usr/local/lib/
-#cp -f artifacts/argon2/libargon2.so /lib/
-#
-#ldconfig
