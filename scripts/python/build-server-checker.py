e73983d201ff4233698bd41278f1f4b4ef06f006
Bwackwat <bwackwat@github.com>
2017-03-03 17:36:04 -0800

    Deployed machines can use the build server checker to find new builds.

---
+++ scripts/python/build-server-checker.py
@@ -0,0 +1,18 @@
+#!/bin/python
+
+import sys, requests, json, time, datetime
+
+branch = sys.argv[1]
+print "Checking commit for latest build for branch " + branch
+commit_file = "artifacts/" + branch + ".latest.commit"
+url = "https://test.bwacwkat.com/build/" + branch + ".latest.commit"
+last_commit = ""
+
+while True:
+	latest_commit = requests.get(url).content
+	if latest_commit != last_commit:
+		with open(commit_file, "w") as f:
+			f.write(latest_commit)
+		last_commit = latest_commit
+		print "Wrote " + latest_commit + " to " + commit_file
+	time.sleep(60)
