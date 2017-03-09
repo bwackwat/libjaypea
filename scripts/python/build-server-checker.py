011d7bf1effe1abf9cdb135307284d16b2ecda55
John Hayes <john.has.come@gmail.com>
2017-03-08 16:25:34 -0800

    Hostnames are not resolvable so early in the runtime UGH gotta have a big error check.

--- scripts/python/build-server-checker.py
+++ scripts/python/build-server-checker.py
@@ -1,6 +1,6 @@
 #!/bin/python
 
-import sys, requests, time, datetime
+import sys, requests, time, datetime, traceback
 
 branch = sys.argv[1]
 print "Checking commit for latest build for branch " + branch
@@ -9,7 +9,14 @@ url = "https://build.bwackwat.com/build/" + branch + ".latest.commit"
 last_commit = ""
 
 while True:
-	latest_commit = requests.get(url).content
+	try:
+		latest_commit = requests.get(url).content
+	except:
+		print "-" * 60
+		print str(datetime.datetime.now())
+		traceback.print_exc(file=sys.stdout)
+		print "-" * 60
+		latest_commit = last_commit
 	if latest_commit != last_commit:
 		with open(commit_file, "w") as f:
 			f.write(latest_commit)
