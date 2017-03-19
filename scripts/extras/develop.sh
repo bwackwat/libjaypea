9cb8a993158d72cc5f40a3b3fafc1da680e03758
John Hayes <john.has.come@gmail.com>
2017-03-18 23:02:22 -0700

    Fixed develop for PROD option.

--- scripts/extras/develop.sh
+++ scripts/extras/develop.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-cd $(dirname "${BASH_SOURCE[0]}")/../../
+set -e
 
 echo "--------------------------------------------------"
-
-wl="scripts/python/watcher.py cpp-source/,scripts/build-library.sh,scripts/build-prefix.sh scripts/build-library.sh $1"
-
-echo $wl
 
 xterm -geometry 100x5 \
 -T "watcher building libjaypea.so" \
--e $wl &
+-e scripts/python/watcher.py cpp-source/,scripts/build-library.sh,scripts/build-prefix.sh "scripts/build-library.sh $1" &
 
 echo "--------------------------------------------------"
 
-wl="scripts/python/watcher.py cpp-source/examples/,artifacts/libjaypea.so,scripts/build-example.sh scripts/build-example.sh $1"
-
-echo $wl
-
 xterm -geometry 100x25 \
 -T "watcher building examples" \
--e $wl &
+-e scripts/python/watcher.py cpp-source/examples/,artifacts/libjaypea.so,artifacts/libjaypeap.so,scripts/build-example.sh "scripts/build-example.sh $1" &
 
 echo "--------------------------------------------------"
