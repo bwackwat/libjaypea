4a1e564b8cdfb22e15ac7e3369f8c9f722b25de6
John Hayes <john.has.come@gmail.com>
2017-04-08 12:13:04 -0700

    Changed default extra keyfile. Changed how receives are handles in the symmetric client.

--- cpp-source/symmetric-tcp-client.cpp
+++ cpp-source/symmetric-tcp-client.cpp
@@ -49,7 +49,7 @@ std::string SymmetricTcpClient::communicate(const char* request, size_t length){
 		if((len = this->encryptor.recv(this->fd, response, PACKET_LIMIT, set_response_callback, &this->reads)) < 0){
 			this->close_client();
 			DEBUG("SymmetricTcpClient recv " << this->fd)
-			return this->communicate(request, length);
+			return response_string;
 		}
 	}while(len == 0);
 
