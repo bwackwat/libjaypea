5004f4c07fd35e06026201a2929d5df1cb6409ed
John Hayes <john.has.come@gmail.com>
2017-04-16 14:08:28 -0700

    Reworked site-templater for a cool recursive nested-templating structure.

--- scripts/python/site-templater.py
+++ scripts/python/site-templater.py
@@ -2,35 +2,41 @@
 
 import os, sys
 
-print "Arguments: %s" % sys.argv
-
-TEMPLATE_NAME = "template.html"
-
 if len(sys.argv) <= 1:
 	print "First argument must be relative directory with templates."
 	sys.exit(1)
 
-dir = sys.argv[1]
-os.stat(dir)
+os.stat(sys.argv[1])
 
-def process_template(dir):
-	template_file = os.path.join(dir, TEMPLATE_NAME)
-	print "Template: " + template_file
-	with open(template_file, "r+") as template:
-		template_text = template.read()
-		#for root, dirs, files in os.walk(dir):
-			#for file in files:
-		for file in os.listdir(dir):
-			file = os.path.join(dir, file)
-			if file.endswith(".template.html"):
-				new_file_name = file.replace(".template.html", ".html")
-				print "Replacing {CONTENT} in %s to %s" % (file, new_file_name)
-				with open(file, "r+") as oldfile, open(new_file_name, "w+") as newfile:
-					newfile.write(template_text.replace("{CONTENT}", oldfile.read()))
+def enter_directory(dir, current_template):
+	items = os.listdir(dir)
+	next_template = ""
+	for item in items:
+		citem = os.path.join(dir, item)
+		if item == "template.html":
+			if next_template != "":
+				print "ERROR FOUND EXTRA TEMPLATE IN " + dir
+				sys.exit(1)
+			with open(citem, "r") as file:
+				next_template = file.read()
+		if item == "nestedtemplate.html":
+			if next_template != "":
+				print "ERROR FOUND EXTRA TEMPLATE IN " + dir
+				sys.exit(1)
+			with open(citem, "r") as file:
+				next_template = current_template.replace("{CONTENT}", file.read())
+	for item in items:
+		citem = os.path.join(dir, item)
+		if citem.endswith(".template.html"):
+			new_item = citem.replace(".template.html", ".html")
+			print "Replacing {CONTENT} in " + citem + " to " + new_item
+			with open(citem, "r") as oldfile, open(new_item, "w+") as newfile:
+				newfile.write(next_template.replace("{CONTENT}", oldfile.read()))
+	for item in items:
+		citem = os.path.join(dir, item)
+		if os.path.isdir(citem):
+			enter_directory(citem, next_template)
 
-for root, dirs, files in os.walk(dir):
-	for file in files:
-		if file == TEMPLATE_NAME:
-			process_template(root)
+enter_directory(sys.argv[1], "{CONTENT}")
 
 print "DONE!"
