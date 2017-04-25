67465d8fe99f7338d7cb569456cb18eb6d776855
bwackwat <bwackwat@github.com>
2017-04-24 21:06:48 -0700

    Made some more SQL changes

--- scripts/extras/tables.sql
+++ scripts/extras/tables.sql
@@ -3,12 +3,15 @@ CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
 	username VARCHAR(50) UNIQUE NOT NULL,
 	password TEXT NOT NULL,
-	token TEXT NOT NULL,
+	primary_token TEXT,
+	secondary_token TEXT,
 	email VARCHAR(100) UNIQUE NOT NULL,
 	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
+	verified BOOLEAN, NOT NULL,
 	created_on TIMESTAMP NOT NULL
 );
+ALTER TABLE users ALTER verified SET DEFAULT FALSE;
 ALTER TABLE users ALTER created_on SET DEFAULT now();
 
 CREATE EXTENSION postgis;
@@ -24,16 +27,6 @@ CREATE TABLE poi (
 );
 ALTER TABLE poi ALTER created_on SET DEFAULT now();
 
-DROP TABLE posts;
-CREATE TABLE posts (
-	id SERIAL PRIMARY KEY,
-	owner_id SERIAL NOT NULL,
-	title TEXT NOT NULL,
-	content TEXT NOT NULL,
-	created_on TIMESTAMP NOT NULL
-);
-ALTER TABLE posts ALTER created_on SET DEFAULT now();
-
 DROP TABLE threads;
 CREATE TABLE threads (
 	id SERIAL PRIMARY KEY,
@@ -47,13 +40,13 @@ CREATE TABLE tags (
 	tag VARCHAR(25) PRIMARY KEY UNIQUE NOT NULL,
 	id SERIAL UNIQUE NOT NULL
 );
-ALTER TABLE threads_tags ALTER created_on SET DEFAULT now();
 
 DROP TABLE messages;
 CREATE TABLE messages (
 	id SERIAL PRIMARY KEY,
 	owner_id SERIAL NOT NULL,
-	message TEXT NOT NULL,
+	title TEXT NOT NULL,
+	content TEXT NOT NULL,
 	created_on TIMESTAMP NOT NULL,
 );
 ALTER TABLE messages ALTER created_on SET DEFAULT now();
@@ -63,7 +56,9 @@ CREATE ROLE bwackwat WITH LOGIN;
 ALTER ROLE bwackwat PASSWORD 'abc123';
 GRANT SELECT, INSERT, UPDATE ON users TO bwackwat;
 GRANT SELECT, INSERT, UPDATE ON poi TO bwackwat;
-GRANT SELECT, INSERT, UPDATE ON posts TO bwackwat;
+GRANT SELECT, INSERT, UPDATE ON threads TO bwackwat;
+GRANT SELECT, INSERT, UPDATE ON tags TO bwackwat;
+GRANT SELECT, INSERT, UPDATE ON messages TO bwackwat;
 
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO bwackwat;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT ON SEQUENCES TO bwackwat;
