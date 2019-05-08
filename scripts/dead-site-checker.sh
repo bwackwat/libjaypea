f6817c9134fa1376a49abd6229c05e603dcf5cf4
John Hayes <john.has.come@gmail.com>
2019-05-08 15:53:24 -0700

    Added --insecure flag to curl call for production usage.

--- scripts/dead-site-checker.sh
+++ scripts/dead-site-checker.sh
@@ -4,7 +4,7 @@ sleep 120
 
 while true
 do
-	STATUS=$(curl -m 5 -s -o /dev/null -w '%{http_code}' $1)
+	STATUS=$(curl -m 5 -s -o /dev/null -w '%{http_code}' $1 --insecure)
 	if [ ! $STATUS -eq 200 ]; then
 		echo "$(date) Restarting site."
 		touch $2
