d5044ed0707e6d3e009f548fc77e72aebe646f7c
John Paul <john.paul.hayes2.lucid@gmail.com>
2020-01-14 19:43:10 +0000

    Improved letsencrypt renew strategy.

--- scripts/letsencrypt-renew.sh
+++ scripts/letsencrypt-renew.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-if [ $# -lt 3 ]; then
-	echo "Usage: $0 </opt/libjaypea> <domain> <port>"
+if [ $# -lt 4 ]; then
+	echo "Usage: $0 </opt/libjaypea> <domain> <port> <key owner username/group>"
 	exit
 fi
+
+set -eux
 
 systemctl stop libjaypea
 
 certbot certonly --standalone --http-01-port $3 --domain $2
 
-cp /etc/letsencrypt/live/$2/fullchain.pem $1/artifacts/
-cp /etc/letsencrypt/live/$2/privkey.pem $1/artifacts/
+cp /etc/letsencrypt/live/$2/fullchain.pem $1/artifacts/ssl.crt
+cp /etc/letsencrypt/live/$2/privkey.pem $1/artifacts/ssl.key
+
+chown $4.$4 $1/artifacts/ssl.crt
+chown $4.$4 $1/artifacts/ssl.key
+
+chmod 644 $1/artifacts/ssl.crt
+chmod 644 $1/artifacts/ssl.key
 
 systemctl start libjaypea
 
